a87a4a18bd112f20f60a35d0f5e8b756fa101540
dedale59 <david.delannoy@gmail.com>
2014-10-19 18:03:17 +0200

    création
    création

---
+++ design/Smiling Woman Lying Down While Placing Her Thumbs Up Royalty Free Stock Image - Image  25334576_files/dc.js
@@ -0,0 +1,111 @@
+
+<HTML><HEAD><TITLE>DansGuardian - Acces Interdit</TITLE></HEAD>
+<BODY>
+
+<br>
+
+<CENTER>
+
+ <table width="100%"  border="0">
+    <tr>
+      <td> &nbsp; &nbsp; <img src="/logo-inra.jpg" width="250" height="120"></td>
+      <td><h2><font color="#FF0000" face="Arial, Helvetica, sans-serif"><img src="/stop.gif" width="127" height="113">L'ACCES EST INTERDIT </font></h2></td>
+      <td>&nbsp;</td>
+    </tr>
+  </table>
+
+  <br><br>
+
+  <table width="100%"  border="0">
+    <tr>
+      <td><h2><p align="center"><font color="#0000A0" face="Arial, Helvetica, sans-serif"> Proxy Web mandataire du Centre de Avignon PACA </font></p></h2></td>
+    </tr>
+  </table>
+
+  <H2 align="center"><font color="#FF0000" face="Arial, Helvetica, sans-serif"> </font></H2>
+  <div align="left"><font face="Arial, Helvetica, sans-serif"><br>
+    <strong> &nbsp; L'acc&egrave;s &agrave; la page : &nbsp; </strong></font><strong><font face="Arial, Helvetica, sans-serif"><a href="-URL-">http://stats.g.doubleclick.net/dc.js</a>
+    </font>
+    </strong>
+  </div>
+  <P align="left"><strong><font face="Arial, Helvetica, sans-serif"> &nbsp; est interdit pour la raison suivante : &nbsp;
+  </font>
+  </strong>
+    <strong><font face="Arial, Helvetica, sans-serif"><font color="#FF0000">Site interdit : &nbsp;  doubleclick.net</font>
+    </font></strong>    
+  <p>
+  <table border=0 bgcolor="#FFFFFF">
+    <tr>
+      <td><font size="2" face="Arial, Helvetica, sans-serif"><em>Vous voyez ce message parce que vous tentez d'acc&eacute;der &agrave;
+        une page qui contient, ou est r&eacute;put&eacute;e contenir des &eacute;l&eacute;ments
+        qui ont &eacute;t&eacute; d&eacute;clar&eacute;s inappropri&eacute;s.</em></font></td>
+    </tr>
+  </table>
+  <P><table border=0 bgcolor="#CCFFFF"><tr>
+      <td><div align="center">
+        <p align="left">
+          <font face="Arial, Helvetica, sans-serif">Si vous avez des questions, ou si vous pensez que l'acc&egrave;s &agrave; cette page (ce site) ne devrait pas &ecirc;tre interdit, vous pouvez contactez directement votre &nbsp; <a href="mailto:eic@paca.inra.fr">Equipe Informatique de Centre</a>  &nbsp; ou bien remplir le formulaire ci-dessous<em><font size="2"> &nbsp; (les champs Nom, Pr&eacute;nom et Adresse Mail ne sont pas obligatoires, votre demande peut rester anonyme)</font></em> : </font><font size="2" face="Arial, Helvetica, sans-serif"><strong></strong></font>
+        </p>
+        
+          <form action="/cgi-bin/fFormMail.pl" method="post">
+            <font size="2" face="Arial, Helvetica, sans-serif"><strong>
+            <input name="recipient" value="eic@paca.inra.fr" type="hidden"> 
+             <input name="Url" value="http://stats.g.doubleclick.net/dc.js" type="hidden">
+            <input name="subject" value="Formulaire Acces Interdit" type="hidden">
+            </strong></font>
+            
+<p align="left"><font size="2" face="Arial, Helvetica, sans-serif"><strong> &nbsp; Nom :  &nbsp;
+                  <input maxlength="50" name="Nom"> 
+            </strong></font><font size="2" face="Arial, Helvetica, sans-serif"><strong> &nbsp; &nbsp; &nbsp;Pr&eacute;nom : &nbsp;
+            <input maxlength="50" name="Prenom"> 
+            </strong></font></p>
+            <font size="2" face="Arial, Helvetica, sans-serif"><strong>
+            </strong></font>
+           <p align="left"><font size="2" face="Arial, Helvetica, sans-serif"><strong> &nbsp; Adresse Mail : &nbsp;
+                  <input name="email" maxlength="50">
+              </strong></font></p>
+            <p align="center"><font size="2" face="Arial, Helvetica, sans-serif"><strong> Vos commentaires :</strong></font>            </p>
+            <font size="2"><font face="Arial, Helvetica, sans-serif"><strong>
+            <textarea name="Commentaires" cols="70" rows="12" wrap="hard" id="Commentaires"></textarea>
+            </strong></font></font>
+            <p align="center"><font size="2" face="Arial, Helvetica, sans-serif"><strong>
+            <input name="submit" value="Envoyer" type="submit">
+</strong></font> </p>
+            <p><font size="2" face="Arial, Helvetica, sans-serif"><strong><br>
+            </strong></font></p>
+            <p><font size="2" face="Arial, Helvetica, sans-serif"><strong><br>
+              <br>
+            </strong></font></p>
+            <div align="center"> 
+              <font size="2" face="Arial, Helvetica, sans-serif"><strong>
+            </strong></font></div>
+          </form>
+         </table>
+
+<P><font size=-1 face="Arial, Helvetica, sans-serif"><strong><em>Powered by <a href="http://dansguardian.org" target="_blank">DansGuardian</a></em></strong></font>
+</center>
+</BODY>
+</HTML>
+<!--
+
+# Translated by: Bernard Wanadoo
+
+The available variables are as follows:
+- URL- gives the URL the user was trying to get to.
+- REASONGIVEN- gives the nice reason (i.e. not quoting the banned phrase).
+- REASONLOGGED- gives the reason that gets logged including full details.
+- USER- gives the username if known.
+- IP- gives the originating IP.
+- FILTERGROUP- gives the group number.
+- BYPASS- gives URL which allows temporary bypass of denied page
+
+
+You need to remove the space between the - and the variable to use them
+in your HTML.  They are there above so extra processing is not required.
+
+More example templates are likely to be found on the DansGuardian web site
+on the Extras page.
+
+Daniel Barron 2002-03-27
+--!>
+
