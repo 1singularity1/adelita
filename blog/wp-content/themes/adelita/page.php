8650d0030bf16f1eb46820ec68abb9fb55283c6b
Delannoy <david.delannoy@gmail.com>
2014-11-09 16:32:42 +0100

    ddd

--- blog/wp-content/themes/adelita/page.php
+++ blog/wp-content/themes/adelita/page.php
@@ -14,9 +14,10 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
+	
 	<div id="content" class="site-content" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+	<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'content', 'page' ); ?>
 
